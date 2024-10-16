814a3b6498bd14734350ad643937858666dc271e
cherub <30589917+lscherub@users.noreply.github.com>
2024-10-15 20:58:51 -0700

    Update index.tsx

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -105,18 +105,18 @@ const Footer = () => {
             <p className="text-sm text-center sm:text-left text-black/60 mb-4 sm:mb-0 sm:mr-1">
               Shop.co © Made by{" "}
               <Link
-                href="https://github.com/mohammadoftadeh"
+                href="https://lscherub.github.io/"
                 className="text-black font-medium"
               >
-                Mohammad Oftadeh
+                Loveleen
               </Link>
               {", "}
               Designed by{" "}
               <Link
-                href="https://www.figma.com/@hamzauix"
+                href="https://lscherub.github.io/"
                 className="text-black font-medium"
               >
-                Hamza Naeem
+                Loveleen
               </Link>
             </p>
             <div className="flex items-center">
